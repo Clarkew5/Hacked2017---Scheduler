3c22dcc39baa8e201d9b832f6ae3f8bfc1026bd0
ClarkeW5 <clarkew5@mymacewan.ca>
2017-01-29 10:03:04 -0700

    took up the sword and slayed a large bug. There was a bigger one.

--- Algorithm/shifts.c
+++ Algorithm/shifts.c
@@ -13,17 +13,16 @@ void parseShiftFile(char *filename, struct Shift *shifts){
 	    double end = atof(strtok(NULL, " "));
 	    int num = atoi(strtok(NULL, " "));
 	    
-	    struct Shift **constraints = calloc(sizeof(struct Shift *), num);
+	    //struct Shift **constraints = calloc(num-1, sizeof(struct Shift *));
 	    
 	    for(int j = 0; j < num; j++){
 		    shifts[i].id = i+1;
-		    shifts[i].day = calloc(1, strlen(day) + 1);
+		    shifts[i].day = calloc(strlen(day) + 1, sizeof(char));
 		    strcpy(shifts[i].day, day);
 		    shifts[i].start = start;
 		    shifts[i].end = end;
 		    shifts[i].n_constraints = num;
-		    constraints[j] = malloc(sizeof(struct Shift));
-		    
+		    shifts[i].constraints = calloc(num, sizeof(struct Shift *));
 #ifdef DEBUG
 		    printf("id: %d\n", shifts[i].id);
 		    printf("day: %s\n", shifts[i].day);
@@ -33,26 +32,17 @@ void parseShiftFile(char *filename, struct Shift *shifts){
 		    i++;
 	    }
 	    
-	    shifts[i].constraints = constraints;
-	    
-	    // fuck you hackers
-	    if (i == 0)
-		    return;
-	    
-	    int subtractor = i - (num - 1);
-	    int index = 0;
-	    
-	    // circular buffer of a cross section of array
-	    for (int j = subtractor; j < i; j++) {
-		    for (int k = 1; k < num; k++) {
-			    shifts[j].constraints[k-1] = &shifts[((j + k) % i) % num + i];
+	    for (int j = i - num; j < i; j++) {
+		    for (int k = 0; k < num-1; k++) {
+			    if(k + i - num == j) j++;
+                shifts[j].constraints[k] = &shifts[j];
 		    }
 	    }
 	    
 	    // People can't work twice in the same day -> add here !!
     }
     i++;
-    shifts[i].day[0]  = '\0';
+    shifts[i].day = '\0';
     
 #ifdef DEBUG 
     printf("------------------------\n\n");
